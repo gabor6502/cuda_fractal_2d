bc7271459c2dc402d593a87e1e0f5671b25aa5ed
gabor6502 <elan.gabor@gmail.com>
2025-07-18 12:28:32 -0500

    prepping for OpenGL interoperation

--- cuda_fractal_2d/main.cpp
+++ cuda_fractal_2d/main.cpp
@@ -1,16 +1,36 @@
 #include "kernel.h"
+#include "graphics.h"
+
 #include <stdio.h>
+
+#define APP_NAME "CudaFractal2D"
 
 int main()
 {
-	printf("-- CudaFractal2D --\n");
+	printf("-- %s --\n", APP_NAME);
 
+	GLFWwindow* window = initOpenGL(APP_NAME);
+	
+	// render
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	/*
 	initCUDA();
 	allocCUDA();
 	setImageSize(800, 800);
 	setIterations(500);
 	runCUDA();
 	deallocCUDA();
+	*/
+
+	glfwTerminate();
 
 	printf("\n-- End of program --\n");
 	return 0;
